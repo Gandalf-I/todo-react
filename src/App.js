d84608e02c927ed8721c39a6d561b45863b39f1a
Ganda1f <danylo.averbakh@lazy-ants.net>
2020-12-18 23:52:56 +0200

    test redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import TodoForm from './components/todoForm';
+import TodoList from './components/todoList';
+import useTodoState from './hooks/useTodoState';
 import './App.css';
 
-function App() {
+const App = () => {
+  const {todos, addTodo, deleteTodo} = useTodoState([]);
+
+  const saveTodo = (text) => {
+    const trimmedText = text.trim();
+
+    if (trimmedText.length > 0) {
+      addTodo(trimmedText);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+
+      <TodoForm
+        saveTodo={saveTodo}
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useInputState.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+const useInputState = () => {
+  const [value, setValue] = useState('');
+
+  return {
+    value,
+    onChange: event => {
+      setValue(event.target.value);
+    },
+    reset: () => setValue('')
+  };
+};
+
+
+export default useInputState
